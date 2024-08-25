67ed0c8f21f806e8061cbbc6bbeec618b2e10e0f
Amit Gupta <amitg.ghy@gmail.com>
2024-08-25 17:00:08 +0530

    fixed config issues

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import {
+  fixupConfigRules,
+  fixupPluginRules,
+  includeIgnoreFile,
+} from '@eslint/compat';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  includeIgnoreFile(gitignorePath),
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'prettier',
+      'plugin:prettier/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:storybook/recommended'
+    )
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+    },
+    ignores: ['dist/', 'node_modules/', 'coverage/', '*.min.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
